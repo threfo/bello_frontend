540dd83a7446beff748147034a7df59b6b8fb279
李广远 <liguangyuan@belloai.com>
2021-11-02 14:45:17 +0800

    fix: 修复auth判断依据问题，package包引用错误问题

--- packages/bello-vue-auth/rollup.config.js
+++ packages/bello-vue-auth/rollup.config.js
@@ -1,7 +1,7 @@
+import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 // 为了将引入的 npm 包，也打包进最终结果中
 import resolve from 'rollup-plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
 
 import pkg from './package.json'
 
