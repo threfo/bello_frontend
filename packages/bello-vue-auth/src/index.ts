68e706fe682a6d9b7bfd9642e55b12c92fbc30ec
李广远 <liguangyuan@belloai.com>
2021-11-26 13:51:31 +0800

    fix: 移除 $store 依赖，改为内部维护

--- packages/bello-vue-auth/src/index.ts
+++ packages/bello-vue-auth/src/index.ts
@@ -4,15 +4,18 @@ export default class Auth {
   authFn: () => false
   routeMap: Map<string, utils._RouteConfig>
   menu: utils.MenuItem[]
+  permissions: string[]
   permissionRouterMap: Map<string, string[]>
   constructor({
     authFn,
     routes,
-    menu
+    menu,
+    permissions
   }: {
     authFn?: any
     routes: utils._RouteConfig[]
     menu: utils.MenuItem[]
+    permissions?: string[]
   }) {
     this.authFn =
       Object.prototype.toString.call(authFn) === '[object Function]'
@@ -22,16 +25,20 @@ export default class Auth {
     this.routeMap = utils.getRouterMapByRouter(routes)
     this.permissionRouterMap = utils.getPermissionMapByRouterMap(this.routeMap)
     this.menu = utils.getMenuByRouteMap(menu, this.routeMap)
+    this.permissions = permissions || []
   }
   static install(Vue): void {
     Vue.prototype.$auth = function (permission: string): boolean {
-      const haveAuth = this.authFn(permission, this.$store)
+      const haveAuth = this.authFn(permission, this.permissions)
       if (!haveAuth) {
         console.warn('没有权限', permission)
       }
 
       return haveAuth
     }
+  }
+  updatePermissions(permissions) {
+    this.permissions = permissions
   }
   updateRouter(routes: utils._RouteConfig[]): utils._RouteConfig[] {
     this.routeMap = utils.getRouterMapByRouter(routes)
