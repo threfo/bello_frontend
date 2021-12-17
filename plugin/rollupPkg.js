5b788809f88987ca95f53284e31621d33408dad9
thomas-ballo <liurongliang@balloai.com>
2021-12-17 19:42:13 +0800

    chore: 修改构建策略

--- plugin/rollupPkg.js
+++ plugin/rollupPkg.js
@@ -1,8 +1,10 @@
 // 为了将引入的 npm 包，也打包进最终结果中
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
+
+import buble from 'rollup-plugin-buble'
 import dts from 'rollup-plugin-dts'
-import { terser } from 'rollup-plugin-terser'
+import { terser } from 'rollup-plugin-terser' // 生成最小化的包
 
 const extensions = ['.ts']
 
@@ -12,7 +14,13 @@ export default ({
   input = './src/index.ts',
   context = 'window'
 }) => {
-  const { version, types } = pkg || {}
+  const { version, types, author } = pkg || {}
+
+  const banner = `/*!
+  * ${name} v${version}
+  * (c) ${new Date().getFullYear()} ${author}
+  * @license MIT
+  */`
 
   const baseOutput = {
     name,
@@ -20,8 +28,12 @@ export default ({
     sourcemap: true,
     footer: `
     if(typeof window !== 'undefined') {
-      window.${name}Version = '${version}'
-    }`
+      window.pgk_${name
+        .replace(/@/g, '_')
+        .replace(/-/g, '_')
+        .replace(/\//g, '_')}_version = '${version}'
+    }
+    ${banner}`
   }
 
   return [
@@ -33,7 +45,12 @@ export default ({
       ],
       plugins: [
         resolve({ extensions }),
-        babel({ extensions, include: ['./src/**/*'] })
+        babel({
+          extensions,
+          include: ['./src/**/*'],
+          runtimeHelpers: true,
+          exclude: 'node_modules/**'
+        })
       ],
       output: [
         {
@@ -43,13 +60,33 @@ export default ({
         },
         {
           ...baseOutput,
-          file: 'lib/index.cjs.js', // package.json 中 "main": "lib/index.cjs.js",
+          plugins: [buble()],
+          sourcemap: false,
+          file: 'lib/index.esm.browser.js',
+          format: 'esm' // umd 兼容形式的包， 可以直接应用于网页 script
+        },
+        {
+          ...baseOutput,
+
+          plugins: [buble(), terser()],
+          file: 'lib/index.esm.browser.min.js',
+          format: 'esm' // umd 兼容形式的包， 可以直接应用于网页 script
+        },
+        {
+          ...baseOutput,
+          file: 'lib/index.common.js', // package.json 中 "main": "lib/index.cjs.js",
           format: 'cjs' // commonjs 形式的包， require 导入
         },
         {
           ...baseOutput,
-          file: 'lib/index.umd.js',
-
+          file: 'lib/index.js',
+          plugins: [],
+          sourcemap: false,
+          format: 'umd' // umd 兼容形式的包， 可以直接应用于网页 script
+        },
+        {
+          ...baseOutput,
+          file: 'lib/index.min.js',
           format: 'umd' // umd 兼容形式的包， 可以直接应用于网页 script
         }
       ],
